945093343cbf8d0742b54545bb750ac0bd6889eb
wompbreaker <wompbreaker@email.com>
2024-11-24 21:03:02 +0100

    Added a configuration file

---
+++ config.py
@@ -0,0 +1,11 @@
+# Camera calibration parameters
+ROWS = 6  
+COLS = 9
+CALIBRATION_IMAGES = 'camera_cal/calibration*.jpg'
+CALIBRATION_DATA_PATH = 'outputs/calibration_data.npz'
+
+# Undistorted image output path
+IMAGE_TO_UNDISTORT = 'test_images/test3.jpg'
+
+# Output path for the undistorted image
+UNDISTORTED_IMAGE_PATH = 'outputs/undistorted.jpg'
