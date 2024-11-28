3cec509f243a040796d73c6fbf4e7592be36a224
wompbreaker <wompbreaker@email.com>
2024-11-28 22:42:02 +0100

    Moved the config file to the utils directory

---
+++ utils/config.py
@@ -0,0 +1,23 @@
+# Camera calibration parameters
+ROWS = 6  
+COLS = 9
+CALIBRATION_IMAGES = 'camera_cal/calibration*.jpg'
+CALIBRATION_DATA_PATH = 'outputs/calibration_data.npz'
+
+BASE_NAME = 'test1'
+STORE_IMAGES = False
+
+# Undistorted image output path
+IMAGE_TO_UNDISTORT = 'test_images/{name}.jpg'
+
+# Output path for the undistorted image
+UNDISTORTED_IMAGE_PATH = 'outputs/undistorted/{name}_undistorted.jpg'
+
+# Output path for the thresholded image
+THRESHOLDED_IMAGE_PATH = 'outputs/thresholded/{name}_thresholded.jpg'
+
+# Output path for the perspective transformed image
+PERSPECTIVE_IMAGE_PATH = 'outputs/warped/{name}_perspective.jpg'
+
+# Project video path
+PROJECT_VIDEO_PATH = 'test_videos/project_video03.mp4'
