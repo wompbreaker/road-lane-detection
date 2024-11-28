858be070d4c9331c8c7ae6773bf6b97fb8a84730
wompbreaker <wompbreaker@email.com>
2024-11-28 22:41:35 +0100

    Created a utils directory to move utility functions from main

---
+++ utils/__init__.py
@@ -0,0 +1,21 @@
+from .logger import setup_logging
+from .misc import *
+from .config import *
+
+__all__ = [
+    "setup_logging",
+    "clear_output_data",
+    "parse_args",
+    "validate_base_name",
+    "ROWS",
+    "COLS",
+    "CALIBRATION_IMAGES",
+    "CALIBRATION_DATA_PATH",
+    "BASE_NAME",
+    "STORE_IMAGES",
+    "IMAGE_TO_UNDISTORT",
+    "UNDISTORTED_IMAGE_PATH",
+    "THRESHOLDED_IMAGE_PATH",
+    "PERSPECTIVE_IMAGE_PATH",
+    "PROJECT_VIDEO_PATH",
+]
