37e7417232bea21a4924ff3e92d47a7aa71974f7
wompbreaker <wompbreaker@email.com>
2024-11-28 01:33:45 +0100

    Added formatting to file name

--- config.py
+++ config.py
@@ -4,8 +4,15 @@ COLS = 9
 CALIBRATION_IMAGES = 'camera_cal/calibration*.jpg'
 CALIBRATION_DATA_PATH = 'outputs/calibration_data.npz'
 
+BASE_NAME = 'test2'
 # Undistorted image output path
-IMAGE_TO_UNDISTORT = 'test_images/test3.jpg'
+IMAGE_TO_UNDISTORT = 'test_images/{name}.jpg'
 
 # Output path for the undistorted image
-UNDISTORTED_IMAGE_PATH = 'outputs/undistorted.jpg'
+UNDISTORTED_IMAGE_PATH = 'outputs/{name}_undistorted.jpg'
+
+# Output path for the thresholded image
+THRESHOLDED_IMAGE_PATH = 'outputs/{name}_thresholded.jpg'
+
+# Project video path
+PROJECT_VIDEO_PATH = 'test_videos/project_video03.mp4'
