54e199bfa998337d154bc323adedd4059472c2fe
wompbreaker <wompbreaker@email.com>
2024-11-30 23:07:41 +0100

    Modified source and destination points for image warping

--- processing/perspective.py
+++ processing/perspective.py
@@ -59,39 +59,9 @@ def _warp_image(
         A bird's-eye view perspective of the image.
     """
     h = _get_homography_matrix(src, dst)
-    
-    return cv.warpPerspective(image, h, (image.shape[1], image.shape[0]))
-
-def _draw_points(
-    image: cv.typing.MatLike, 
-    points: np.ndarray, 
-    color: tuple = (0, 255, 0)
-) -> cv.typing.MatLike:
-    """Draw points on an image.
-
-    Draw points on an image using the specified color. The points are drawn
-    as circles with a radius of 8 pixels.
-
-    Parameters
-    ----------
-    image : cv.typing.MatLike
-        The image to draw the points on.
-
-    points : np.ndarray
-        The points to draw on the image.
-
-    color : tuple, optional
-        The color of the points, by default (0, 255, 0).
-
-    Returns
-    -------
-    cv.typing.MatLike
-        The image with the points drawn on it.
-    """
-    for point in points:
-        cv.circle(image, (int(point[0]), int(point[1])), 8, color, -1)
-
-    return image
+    height = image.shape[0]
+    width = image.shape[1]
+    return cv.warpPerspective(image, h, (width, height))
 
 @utils.timer
 def perspective_transform(binary_image: cv.typing.MatLike) -> cv.typing.MatLike:
@@ -115,26 +85,21 @@ def perspective_transform(binary_image: cv.typing.MatLike) -> cv.typing.MatLike:
     """
     log.info("Applying perspective transform...")
     image = cv.cvtColor(binary_image, cv.COLOR_BGR2RGB)
-    image_width = image.shape[1]
-    image_height = image.shape[0]
-    line_dst_offset = 180
 
     # Source points
-    top_left = (image_width * 0.44, image_height * 0.65)
-    top_right = (image_width * 0.58, image_height * 0.65)
-    bottom_right = (image_width * 0.9, image_height)
-    bottom_left = (image_width * 0.25, image_height)
-    src = [top_left, top_right, bottom_right, bottom_left]
+    top_right = (731, 477)
+    bottom_right = (1056, 689)
+    bottom_left = (260, 689)
+    top_left = (556, 477)
+    src = [top_right, bottom_right, bottom_left, top_left]
 
     # Destination points
-    dst = [
-        [src[3][0] + line_dst_offset, 0],
-        [src[2][0] - line_dst_offset, 0],
-        [src[2][0] - line_dst_offset, image_height],
-        [src[3][0] + line_dst_offset, image_height]
-    ]
+    top_right = (900, 0)
+    bottom_right = (900, 689)
+    bottom_left = (250, 689)
+    top_left = (250, 0)
+    dst = [top_right, bottom_right, bottom_left, top_left]
 
-    # image = _draw_points(image.copy(), src)
     image = _warp_image(image, src, dst)
     log.info("Perspective transform applied.")
 
