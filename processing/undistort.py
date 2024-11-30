6bce7936e8c85d71cf0fdc33d269dca5ac06319f
wompbreaker <wompbreaker@email.com>
2024-11-30 23:05:10 +0100

    Removed getOptimalNewCameraMatrix

--- processing/undistort.py
+++ processing/undistort.py
@@ -8,6 +8,11 @@ import utils
 
 log = logging.getLogger("Undistort")
 
+# Load the calibration data
+with np.load(utils.CALIBRATION_DATA_PATH) as data:
+    matrix = data['mtx']
+    dist_coeffs = data['dist']
+    
 @utils.timer
 def undistort_image(image_name: str = utils.BASE_NAME) -> cv.typing.MatLike:
     """Undistort an image using camera calibration parameters.
@@ -32,27 +37,26 @@ def undistort_image(image_name: str = utils.BASE_NAME) -> cv.typing.MatLike:
     log.info('Undistorting the image...')
     if not os.path.exists(utils.CALIBRATION_DATA_PATH):
         raise FileNotFoundError("Camera calibration data not found.")
-    # Load the calibration data
-    with np.load(utils.CALIBRATION_DATA_PATH) as data:
-        matrix = data['mtx']
-        dist_coeffs = data['dist']
 
     # Read the image to be undistorted
-    img = cv.imread(utils.IMAGE_TO_UNDISTORT.format(name=image_name))
-    height, width = img.shape[:2]
+    image = cv.imread(utils.IMAGE_TO_UNDISTORT.format(name=image_name))
+    height = image.shape[0]
+    width = image.shape[1]
+    if height != 720 or width != 1280:
+        image = cv.resize(image, (1280, 720))
 
     # Get the optimal camera matrix for better undistortion
     # ROI: Region of Interest
-    new_matrix, roi = cv.getOptimalNewCameraMatrix(
-        matrix, dist_coeffs, (width, height), 1, (width, height)
-    )
+    # matrix, roi = cv.getOptimalNewCameraMatrix(
+    #     matrix, dist_coeffs, (width, height), 1, (width, height)
+    # )
 
     # Undistort the image
-    undistorted_image = cv.undistort(img, matrix, dist_coeffs, None, new_matrix)
+    undistorted_image = cv.undistort(image, matrix, dist_coeffs, None, matrix)
 
     # Crop the image based on the region of interest
-    x, y, width, height = roi
-    undistorted_image = undistorted_image[y:y+height, x:x+width]
+    # x, y, width, height = roi
+    # undistorted_image = undistorted_image[y:y+height, x:x+width]
 
     # Return the undistorted image
     return undistorted_image
