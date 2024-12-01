169a00b1b7d9e73de215d5dc5552d120f59e31c7
wompbreaker <wompbreaker@email.com>
2024-12-02 00:27:45 +0100

    Added video literals

--- utils/config.py
+++ utils/config.py
@@ -4,7 +4,8 @@ COLS = 9
 CALIBRATION_IMAGES = 'camera_cal/calibration*.jpg'
 CALIBRATION_DATA_PATH = 'outputs/calibration_data.npz'
 
-BASE_NAME = 'test1'
+BASE_IMAGE_NAME = 'test1'
+BASE_VIDEO_NAME = 'project_video01'
 STORE_IMAGES = False
 
 # Undistorted image output path
@@ -20,4 +21,4 @@ THRESHOLDED_IMAGE_PATH = 'outputs/thresholded/{name}_thresholded.jpg'
 PERSPECTIVE_IMAGE_PATH = 'outputs/warped/{name}_perspective.jpg'
 
 # Project video path
-PROJECT_VIDEO_PATH = 'test_videos/project_video03.mp4'
+PROJECT_VIDEO_PATH = 'test_videos/{name}.mp4'
